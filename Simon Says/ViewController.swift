9b54b111e22fc487ad22e6f1472f6372e66c35cf
Douglas Schiavi <schiavi.douglas@gmail.com>
2021-06-02 11:27:05 -0300

    Create project files and first setups

--- Simon Says/ViewController.swift
+++ Simon Says/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorButtons: [CircularButton]!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet var playerLabels: [UILabel]!
+    @IBOutlet var scoreLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorButtons = colorButtons.sorted() { $0.tag < $1.tag }
+        playerLabels = playerLabels.sorted() { $0.tag < $1.tag }
+        scoreLabels = scoreLabels.sorted() { $0.tag < $1.tag }
+        
+        createNewGame()
     }
 
-
+    @IBAction func colorButtonHandler(_ sender: CircularButton) {
+        print("Button \(sender.tag) tapped")
+    }
+    
+    @IBAction func actionButtonHandler(_ sender: UIButton) {
+        print("Action Button")
+    }
+    
+    func createNewGame() {
+        actionButton.setTitle("Start Game", for: .normal)
+        
+        for button in colorButtons {
+            button.alpha = 0.5
+        }
+    }
+    
 }
 
